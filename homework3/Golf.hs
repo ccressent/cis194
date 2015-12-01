354b5c2e0f351819f66677e3d5c0443424665249
Cyril Cressent <cyril@cressent.org>
2015-11-30 23:06:10 -0800

    Add exercise2 for homework3

--- homework3/Golf.hs
+++ homework3/Golf.hs
@@ -9,3 +9,11 @@ every n xs = (take 1 . drop (n-1)) xs ++ every n (drop n xs)
 -- (`every` xs) :: Int -> [a]
 skips :: [a] -> [[a]]
 skips xs = map (`every` xs) [1..(length xs)]
+
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima l
+    | length l < 3 = []
+localMaxima (x:y:z:xs)
+    | y > x && y > z = y : localMaxima (y:z:xs)
+    | otherwise      = localMaxima (y:z:xs)
