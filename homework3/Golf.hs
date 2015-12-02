d9ea3afea2b12fcec08fe7c893c56d22fa84433c
Cyril Cressent <cyril@cressent.org>
2015-12-02 10:27:27 -0800

    Add exercise3 for homework3

--- homework3/Golf.hs
+++ homework3/Golf.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import Data.List
+
 every :: Int -> [a] -> [a]
 every _ [] = []
 every n xs = (take 1 . drop (n-1)) xs ++ every n (drop n xs)
@@ -17,3 +19,24 @@ localMaxima l
 localMaxima (x:y:z:xs)
     | y > x && y > z = y : localMaxima (y:z:xs)
     | otherwise      = localMaxima (y:z:xs)
+
+
+histogram :: [Int] -> String
+histogram xs = bars r ++ "==========\n0123456789\n"
+    where r = map (\xs -> (head xs, length xs)) $ group $ sort xs
+
+bars :: [(Int, Int)] -> String
+bars [] = []
+bars ps = bars (decrement ps) ++ formatLine ps ++ "\n"
+
+formatLine :: [(Int, Int)] -> String
+formatLine [] = ""
+formatLine ps = map (\x -> case lookup x ps of
+    Nothing -> ' '
+    Just _  -> '*') [0..9]
+
+decrement :: [(Int, Int)] -> [(Int, Int)]
+decrement []       = []
+decrement ((a,b):xs)
+    | b > 1     = (a, b-1) : decrement xs
+    | otherwise = decrement xs
