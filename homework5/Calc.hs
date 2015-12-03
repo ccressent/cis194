9fdf2e4a7de2d23a8d3f68cd1fb070cb0d87b88b
Cyril Cressent <cyril@cressent.org>
2015-12-02 19:06:43 -0800

    Add exercise4 for homework5

--- homework5/Calc.hs
+++ homework5/Calc.hs
@@ -3,6 +3,9 @@ module Calc where
 import ExprT
 import Parser
 
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7   = Mod7   Integer deriving (Eq, Show)
+
 class Expr a where
     lit :: Integer -> a
     add :: a -> a -> a
@@ -12,6 +15,36 @@ instance Expr ExprT where
     lit = Lit
     add = Add
     mul = Mul
+
+instance Expr Integer where
+    lit = id
+    add = (+)
+    mul = (*)
+
+instance Expr Bool where
+    lit = (> 0)
+    add = (||)
+    mul = (&&)
+
+instance Expr MinMax where
+    lit = MinMax
+    add (MinMax a) (MinMax b) = MinMax (max a b)
+    mul (MinMax a) (MinMax b) = MinMax (min a b)
+
+instance Expr Mod7 where
+    lit = Mod7
+    add (Mod7 a) (Mod7 b) = Mod7 ((a + b) `mod` 7)
+    mul (Mod7 a) (Mod7 b) = Mod7 ((a * b) `mod` 7)
+
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool    = testExp :: Maybe Bool
+testMM      = testExp :: Maybe MinMax
+testSat     = testExp :: Maybe Mod7
+
 
 eval :: ExprT -> Integer
 eval (Lit n)           = n
