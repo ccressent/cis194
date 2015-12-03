302af8d8c251328ded22b98c8e499be97e3458ee
Cyril Cressent <cyril@cressent.org>
2015-12-02 18:39:21 -0800

    Add exercise2 of homework5

--- homework5/Calc.hs
+++ homework5/Calc.hs
@@ -1,8 +1,14 @@
 module Calc where
 
 import ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit n)           = n
 eval (Add expr1 expr2) = eval expr1 + eval expr2
 eval (Mul expr1 expr2) = eval expr1 * eval expr2
+
+evalStr :: String -> Maybe Integer
+evalStr s = case parseExp Lit Add Mul s of
+    Nothing   -> Nothing
+    Just expr -> Just (eval expr)
