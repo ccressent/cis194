b74ced6ce777524606a02953354ea4576857b3fa
Cyril Cressent <cyril@cressent.org>
2015-12-06 15:48:52 -0800

    Add exercise2.1 for homework7

--- homework7/JoinList.hs
+++ homework7/JoinList.hs
@@ -2,6 +2,8 @@ module JoinList where
 
 import Data.Monoid
 
+import Sized
+
 data JoinList m a = Empty
                   | Single m a
                   | Append m (JoinList m a) (JoinList m a)
@@ -14,3 +16,43 @@ tag :: Monoid m => JoinList m a -> m
 tag Empty          = mempty
 tag (Single m _)   = m
 tag (Append m _ _) = m
+
+sizeOf :: (Sized a) => a -> Int
+sizeOf = getSize . size
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ Empty        = Nothing
+indexJ 0 (Single _ x) = Just x
+indexJ _ (Single _ _) = Nothing
+indexJ n jl@(Append _ left right)
+    | n >= sizeOf (tag jl)   = Nothing
+    | n <  sizeOf (tag left) = indexJ n left
+    | otherwise              = indexJ (n - sizeOf (tag left)) right
+
+
+--
+-- Everything below used to run manual tests in the REPL
+--
+
+(!!?) :: [a] -> Int -> Maybe a
+[] !!? _         = Nothing
+_  !!? i | i < 0 = Nothing
+(x:_) !!? 0      = Just x
+(_:xs) !!? i     = xs !!? (i-1)
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty              = []
+jlToList (Single _ a)       = [a]
+jlToList (Append _ jl1 jl2) = jlToList jl1 ++ jlToList jl2
+
+testJL :: JoinList Size Int
+testJL = Append (Size 4) (Append (Size 2) (Single (Size 1) 0) (Single (Size 1) 1)) (Append (Size 2) (Single (Size 1) 2) (Single (Size 1) 3))
+
+testJL2 :: JoinList Size Char
+testJL2 = Append (Size 4)
+            (Append (Size 3)
+              (Single (Size 1) 'y')
+              (Append (Size 2)
+                (Single (Size 1) 'e')
+                (Single (Size 1) 'a')))
+            (Single (Size 1) 'h')
