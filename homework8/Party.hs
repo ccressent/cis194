b6a13eca3320a5566108f3908e714e61dde74de0
Cyril Cressent <cyril@cressent.org>
2015-12-10 09:40:51 -0800

    Add exercise1.2 for homework8

--- homework8/Party.hs
+++ homework8/Party.hs
@@ -1,6 +1,12 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Party where
 
 import Employee
+
+instance Monoid GuestList where
+    mempty = GL [] 0
+    mappend (GL xs fun1) (GL ys fun2) = GL (xs ++ ys) (fun1 + fun2)
 
 glCons :: Employee -> GuestList -> GuestList
 glCons x (GL xs fun) = GL (x : xs) (fun + empFun x)
