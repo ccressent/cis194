ade608dde0e9bc367f42dd4439a8a876bb0c4962
Cyril Cressent <cyril@cressent.org>
2016-03-07 23:53:49 -0800

    Add exercises 1 to 4 for homework12

--- homework12/Risk.hs
+++ homework12/Risk.hs
@@ -2,12 +2,14 @@
 
 module Risk where
 
+import Control.Monad
 import Control.Monad.Random
+import Data.List (sort)
 
 ------------------------------------------------------------
 -- Die values
 
-newtype DieValue = DV { unDV :: Int } 
+newtype DieValue = DV { unDV :: Int }
   deriving (Eq, Ord, Show, Num)
 
 first :: (a -> b) -> (a, c) -> (b, c)
@@ -26,3 +28,35 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+  deriving Show
+
+
+casualty :: DieValue -> DieValue -> (Army, Army)
+casualty att def
+  | att > def = (0,1)
+  | otherwise = (1,0)
+
+casualties :: [DieValue] -> [DieValue] -> (Army, Army)
+casualties attacks defenses = (attackers_dead, defenders_dead)
+  where attackers_dead = sum $ map fst results
+        defenders_dead = sum $ map snd results
+        results = zipWith casualty attacks defenses
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle bf = do
+            attacks  <- liftM (reverse . sort) $ replicateM n_attacks die
+            defenses <- liftM (reverse . sort) $ replicateM n_defenses die
+            return $ Battlefield (attackers bf - fst (casualties attacks defenses)) (defenders bf - snd (casualties attacks defenses))
+  where n_attacks  = min 3 $ max 0 (attackers bf - 1)
+        n_defenses = min 2 (defenders bf)
+
+invade :: Battlefield -> Rand StdGen Battlefield
+invade bf
+    | defenders bf == 0 = return bf
+    | attackers bf  < 2 = return bf
+    | otherwise         = battle bf >>= invade
+
+successProb :: Battlefield -> Rand StdGen Double
+successProb bf = liftM ((/1000) . victories) $ replicateM 1000 $ invade bf
+  where attackersWon x = defenders x == 0
+        victories      = fromIntegral . length . filter attackersWon
